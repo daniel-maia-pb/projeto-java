9b59e6a6340f8fad7bce4a42997c9c8a1a773646
Daniel Azevedo <daniel.azevedo.maia@hotmail.com>
2024-01-14 20:52:00 -0300

    Criação de classe Pessoa e refatoração da Main

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
-        // Lendo o nome:
-        System.out.print("Digite seu nome: ");
-        String nome = scanner.nextLine();
-
-        // Lendo a idade:
-        System.out.print("Digite sua idade: ");
-        int idade = scanner.nextInt();
-
-        System.out.println("Nome: " + nome + ", Idade: " + idade);
-        System.out.println("Até a próxima!");
-
-        scanner.close();
+        var pessoa = new Pessoa("Daniel", "32");
 
     }
 }
